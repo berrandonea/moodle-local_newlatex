e2309a19731ef75747817862e96354fc6924f812
Brice Errandonea <berrandonea@yahoo.fr>
2018-11-24 21:23:51 +0100

    Create version.php

---
+++ version.php
@@ -0,0 +1,30 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Updates Moodle TeX notation in the current course.
+ *
+ * @package   local_newlatex
+ * @copyright 2018 Brice Errandonea <brice.errandonea@u-cergy.fr>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ * File : version.php
+ * Version number
+ */
+
+$plugin->version = 2018112400;
+$plugin->requires = 2015051100;
+$plugin->component = 'local_newlatex';
