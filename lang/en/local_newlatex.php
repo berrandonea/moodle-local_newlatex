38cb9840421cd68a04c867a98e3b977a84a07505
Brice Errandonea <berrandonea@yahoo.fr>
2018-11-24 22:37:09 +0100

    Create local_newlatex.php

---
+++ lang/en/local_newlatex.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Updates Moodle TeX notation in the current course.
+ *
+ * @package   local_newlatex
+ * @copyright 2018 Brice Errandonea <brice.errandonea@u-cergy.fr>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ * File : lang/en/local_newlatex.php
+ * English language file
+ */
+
+$string['pluginname'] = "New LaTeX convertor";
+$string['convert'] = "Update TeX notation";
+$string['newlatex:convert'] = "Update TeX notation";
+$string['thiswillconvert'] = "On previous Moodle versions, LaTeX expressions where surrounded by $$ symbols. Now, the surrounding symbols are \( and \). If this course comes from another platform, with an older Moodle version, you might want to update your expressions.";
+$string['updatedtexts'] = "updated texts";
